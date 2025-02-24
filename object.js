ef95ea6863e2ceaefb2fe90acd3859a34955abe7
Your Name <veronichka.danilina@mail.ru>
2025-02-25 00:10:16 +0400

    feat: добавлен метод getSortDescendingOrderFields, кот. возвращает массив ключей  имеющих числовое значение, отсортированных по убыванию

--- object.js
+++ object.js
@@ -1,5 +1,3 @@
-//метод, который на вход получает объект, а на выходе возвращает число, соответствующее сумме всех числовых полей
-
 let user = {
  name: 'Maria',
  surname: 'Ivanova',
@@ -7,7 +5,8 @@ let user = {
  age: 22,
  projects: 7,
  karma: 17,
- 
+
+ //метод, который на вход получает объект, а на выходе возвращает число, соответствующее сумме всех числовых полей
  getNumberNumericFields(obj) {
   const valuesArr = Object.values(obj);
   const numericValues = valuesArr.filter((value) => typeof value === 'number');
@@ -16,6 +15,25 @@ let user = {
   }, 0);
   console.log(sumNumericValues);
  },
+
+ // метод возвращает массив с названиями полей,где названия полей отсортированы по убыванию
+ getSortDescendingOrderFields(obj) {
+  let arr = [];
+  for (let key in obj) {
+   if (typeof obj[key] === 'number') {
+    arr.push([key, obj[key]]);
+   }
+  }
+  arr.sort((a, b) => {
+   return b[1] - a[1];
+  });
+
+  const objKeys = Object.fromEntries(arr);
+
+  console.log(Object.keys(objKeys));
+  return Object.keys(objKeys);
+ },
 };
 
 user.getNumberNumericFields(user);
+user.getSortDescendingOrderFields(user);
